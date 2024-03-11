fa0dc7c24161a37d056bf1a048caab421b2c4942
hugobler <hugo.ernesto@ouribank.com.br>
2024-03-11 10:46:09 -0300

    initial commit

---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+const Header = (): React.JSX.Element => {
+  return (
+    <header className="min-h-[10vh] bg-secondary mb-4 text-white flex items-center px-4 pt-10 pb-4">
+      <h1 className="text-title text-5xl">POC: Single-SPA</h1>
+    </header>
+  );
+};
+
+export default Header;
